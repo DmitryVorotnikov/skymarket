ae80ad2bb9dc355902bf48e4a1159622ba62b34e
DmitryVorotnikov <vorotnikov7009@gmail.com>
2024-01-28 19:35:41 +0800

    Added environment variable handling to settings.py

--- config/settings.py
+++ config/settings.py
@@ -10,11 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+from datetime import timedelta
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
+
+# Type of environment
+ENV_TYPE = os.getenv('ENV_TYPE')  # local, test, stage, prod
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
